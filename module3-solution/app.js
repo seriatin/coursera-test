f5499168d31ff2463a47bab22418b224f8fe6d72
unknown <>
2017-02-05 12:54:45 +0900

    Module3 Solution Add

---
+++ module3-solution/app.js
@@ -0,0 +1,77 @@
+(function() {
+  'use strict';
+
+  angular.module('NarrowItDownApp', [])
+  .controller('NarrowItDownController', NarrowItDownController)
+  .service('MenuSearchService', MenuSearchService)
+  .directive('foundItems', FoundItemsDirective)
+  .constant('ApiBasePath', 'https://davids-restaurant.herokuapp.com');
+
+  function FoundItemsDirective() {
+    var ddo = {
+      restrict: 'E',
+      templateUrl: 'foundItems.html',
+      scope: {
+        foundItems: '<',
+        onRemove: '&'
+      },
+      controller: FoundItemsDirectiveController,
+      controllerAs: 'items',
+      bindToController: true
+    };
+    return ddo;
+  }
+
+  function FoundItemsDirectiveController() {
+    var items = this;
+  }
+
+  NarrowItDownController.$inject = ['MenuSearchService'];
+  function NarrowItDownController(MenuSearchService) {
+
+    var narrowItDown = this;
+    narrowItDown.searchTerm = '';
+    narrowItDown.found = [];
+
+    narrowItDown.getMatchedMenuItems = function() {
+      var promise = MenuSearchService.getMatchedMenuItems(narrowItDown.searchTerm);
+      promise.then(function(foundItems) {
+        narrowItDown.found = foundItems;
+      })
+      .catch(function(error) {
+        console.log(error);
+      });
+
+    }
+
+    narrowItDown.removeItem = function(index) {
+      narrowItDown.found.splice(index, 1);
+    }
+
+  }
+
+
+
+
+
+  MenuSearchService.$inject = ['$http', 'ApiBasePath'];
+  function MenuSearchService($http, ApiBasePath) {
+
+    var service = this;
+
+    service.getMatchedMenuItems = function(searchTerm) {
+      return $http({
+        method: 'GET',
+        url: ApiBasePath + '/menu_items.json'
+      })
+      .then(function(result) {
+        var foundItems = result.data.menu_items.filter(function(arg) {
+          return (arg.description.indexOf(searchTerm) != -1);
+        });
+        return foundItems;
+      });
+    }
+
+  }
+
+})();
